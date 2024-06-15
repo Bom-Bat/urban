e6a384256a47ae1b392b6113feed52100f90c90d
Bom-Bat <70853577+Bom-Bat@users.noreply.github.com>
2024-06-15 17:37:54 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kol_vo_DZ = 12
+vremya_na_DZ = 1.5
+kurs = '"Python"'
+vremya_na1_DZ = vremya_na_DZ / kol_vo_DZ
+print('\n Курс:', kurs, ', всего задач:', kol_vo_DZ, ' затрачено часов:', vremya_na_DZ, ' среднее время выполнения ', vremya_na1_DZ, ' часа')
