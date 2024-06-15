278aad5621d25bec946aa298c469219598159051
Bom-Bat <70853577+Bom-Bat@users.noreply.github.com>
2024-06-15 18:48:34 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('\nВведите произвольную строку: ')
+print('\nКолличество символов в строке: ', len(my_string))
+print('\nСтрока в верхнем регистре: ', my_string.upper())
+print('\nСтрока в нижнем регистре: ', my_string.lower())
+print('\nСтрока без пробелов: ', my_string.replace(' ', ''))
+print('\nПервый символ строки: ', my_string[0])
+print('\nПоследний символ строки: ', my_string[-1])
