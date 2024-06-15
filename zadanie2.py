5eebf9425a28aad3adecd5a3c95e998a87b5190d
Bom-Bat <70853577+Bom-Bat@users.noreply.github.com>
2024-06-15 12:52:41 +0300

    Add files via upload

---
+++ zadanie2.py
@@ -0,0 +1,21 @@
+a = '3.1415926 как интересно'
+print('\n')
+print('Длинна строки а', len(a), '\n')
+first = 2
+second = 5
+summa = first + second
+diff = first - second
+print('сумма ', summa, 'разность', diff, '\n')
+first = 4.3
+second = 2.7
+third = 3.1
+mean = (first + second + third)/3
+print('Среднее арифметическое', mean, '\n')
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string, first_string, '\n')
+a = 5
+b = 8
+c = 4
+f = (((a * b) + (a * c))**3)/2
+print('результат вычислений', int(f))
