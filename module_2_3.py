56dbbce1ad333858d9aedcc15d3c7bb952820d09
Bom-Bat <70853577+Bom-Bat@users.noreply.github.com>
2024-06-19 20:46:15 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = len(my_list)
+i = 0
+while i < a:
+    i +=1
+    if my_list[i] > 0:
+        print(f'\n\tЧисло {my_list[i]} положительное')
+    elif my_list[i] == 0:
+        continue
+    else:
+        print('\n\tВстретили отрицательное число \n\n\t\tС Т О П !')
+        break
+print('\n\tРасчет окончен')
